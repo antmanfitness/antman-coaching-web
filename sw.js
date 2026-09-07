140f02b084c51b5e0e033e9a52be286bf5dd6f73
ANTMANFITNESS <antmanfitness@gmail.com>
2026-09-07 03:36:54 +0000

    Refresh Pages SPA fallback

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antman-spacing-v7'
+const CACHE = 'antman-spacing-v8'
 const BASE = new URL('./', self.registration.scope).pathname
 const INDEX = new URL('./index.html', self.registration.scope).pathname
 const PRECACHE = [
