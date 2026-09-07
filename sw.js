7a5c78be55c841e6fbf22965550a7957255f2dad
ANTMANFITNESS <antmanfitness@users.noreply.github.com>
2026-09-07 01:23:01 +0000

    Deploy neutral Client Hub sign-in refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antman-spacing-v3'
+const CACHE = 'antman-spacing-v4'
 const BASE = new URL('./', self.registration.scope).pathname
 const INDEX = new URL('./index.html', self.registration.scope).pathname
 const PRECACHE = [
