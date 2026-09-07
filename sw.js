bb6dd718619b3d8fd54f08d9e7f81614778b33c3
ANTMANFITNESS <antmanfitness@gmail.com>
2026-09-07 03:58:39 +0000

    Deploy derived macro calorie targets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'antman-spacing-v10'
+const CACHE = 'antman-spacing-v11'
 const BASE = new URL('./', self.registration.scope).pathname
 const INDEX = new URL('./index.html', self.registration.scope).pathname
 const PRECACHE = [
